a1cbe8ca244357354bfe07495ac24b6010b86a7a
cubeorange4 <cubeorange4@gmail.com>
2024-02-03 10:04:08 +0900

    編集

--- kadai_002/nagoyameshi/src/main/java/com/example/nagoyameshi/controller/MembershipController.java
+++ kadai_002/nagoyameshi/src/main/java/com/example/nagoyameshi/controller/MembershipController.java
@@ -1,13 +1,27 @@
 package com.example.nagoyameshi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.nagoyameshi.service.StripeService;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class MembershipController {
 	
+	private final StripeService stripeService;
+	
+	public MembershipController(StripeService stripeService) {
+		this.stripeService = stripeService;
+	}
+	
 	@GetMapping("/membership")
-	public String index() {
+	public String index(HttpServletRequest httpServletRequest, Model model) {
+		String sessionId = stripeService.createStripeSession(httpServletRequest);
+		
+		model.addAttribute("sessionId", sessionId);
 		
 		return "membership/index";
 	}
